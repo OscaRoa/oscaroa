b8811eac0a62cecfee369f6c662b85f0c016522f
Oscar Roa <oshardy3@gmail.com>
2015-08-26 21:49:35 -0500

    Landing app added. Basic structure set up.

--- personalWebsite/urls.py
+++ personalWebsite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from landing import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.landingPage, name='home')
 ]
